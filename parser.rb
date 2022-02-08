0b775a2fc6327d7fc92413ab8d0b4cdca7e68a76
Rob <rob.eagleheart@gmail.com>
2022-02-08 17:12:57 +0000

    Number of visits functionality + passing test

--- parser.rb
+++ parser.rb
@@ -1,1 +1,7 @@
 require_relative "analysis.rb"
+
+parser = Parser.new # New parser
+if parser.load_file # load and check if file exist
+  puts "Number of visitors"
+  puts parser.number_of_visits # display number of visitors
+end
